13cf80d68b397dcde7f7bc6131003d88138f65fc
Matheus Messias <matheus.jf@live.com>
2023-08-03 19:00:57 -0300

    add new routes"

--- src/app.ts
+++ src/app.ts
@@ -26,3 +26,67 @@ app.post('/book/create', async (req, res) => {
 
   return res.status(201).send()
 })
+
+app.get('/books', async (req, res) => {
+  const books = await prisma.book.findMany({
+    select: {
+      author: true,
+      id: true,
+      cover_url: true,
+      Rating: true,
+    },
+  })
+
+  return res.status(200).send(books)
+})
+
+app.get('/books/popular', async (_req, res) => {
+  const popularRatings = await prisma.rating.findMany({
+    orderBy: {
+      rate: 'desc',
+    },
+    distinct: ['book_id'],
+    take: 5,
+  })
+
+  const books = await prisma.book.findMany({
+    where: {
+      id: {
+        in: popularRatings.map((rating) => rating.book_id),
+      },
+    },
+    include: {
+      Rating: true,
+    },
+  })
+
+  books.sort((a, b) => {
+    const rateA =
+      popularRatings.find((rating) => rating.book_id === a.id)?.rate || 0
+    const rateB =
+      popularRatings.find((rating) => rating.book_id === b.id)?.rate || 0
+    return rateB - rateA
+  })
+
+  return res.status(200).send(books)
+})
+
+app.post('/rating', async (req, res) => {
+  const ratingBodySchema = z.object({
+    rate: z.coerce.number().min(1),
+    description: z.string(),
+    bookId: z.string(),
+  })
+
+  const { bookId, description, rate } = ratingBodySchema.parse(req.body)
+
+  await prisma.rating.create({
+    data: {
+      description,
+      rate,
+      book_id: bookId,
+    },
+  })
+
+  return res.status(201).send()
+})
